9bbf663a5d3bca662d35e1f9a1aefff7135c6536
bharathaladandi <bharathaladandi@gmail.com>
2023-11-24 17:18:52 +0530

    Top main session created

--- ntwist/src/Components/HomePage.jsx
+++ ntwist/src/Components/HomePage.jsx
@@ -4,10 +4,23 @@ import { Navbar } from './Navbar';
 import '../Styles/HomePage.css'
 
 export const HomePage = () => {
-    return(
-        <div>
+    return (
+        <div className='container'>
+
             <div><Navbar /></div>
-            <div>HomePage</div>
+            <div className='innercontainer'>
+                    <div className="mainpage">
+                        <div className="left">
+                            <h1>Data-powered solution for Industrial Excellence</h1>
+                            <div>
+                                <button>Read More</button>
+                            </div>
+                        </div >
+                        <div className="right">
+                            <img src="https://ntwist.com/wp-content/uploads/2023/01/home-image-icons-v6.png" alt="" />
+                        </div>
+                    </div>
+            </div>
         </div>
     )
 }
--- ntwist/src/Components/Navbar.jsx
+++ ntwist/src/Components/Navbar.jsx
@@ -3,21 +3,30 @@ import '../Styles/Navbar.css';
 
 
 export const Navbar = () => {
-    return (
-        <nav className="navbar">
-            <div className="left">
-                <img className='logo' src="https://ntwist.com/wp-content/uploads/2021/12/ntwistlight.png" alt="" />
-            </div>
-            <div className='right'>
-            
-                <ul className="rightinner">
-                    <li><a href="">Home</a></li>
-                    <li><a href="">Industries</a></li>
-                    <li><a href="">AI Software</a></li>
-                    <li><a href="">Blog</a></li>
-                    <li><a href="">Contact Us</a></li>
-                </ul>
-            </div>
-        </nav>
-    )
+    const [MenuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+      setMenuOpen(!MenuOpen);
+    };
+  return (
+    <nav className="navbar">
+        <div className="left">
+        <img className='logo' src="https://ntwist.com/wp-content/uploads/2021/12/ntwistlight.png" alt="" />
+      </div>
+        <div className={`right ${MenuOpen ? 'open' : ''}`}>
+        <div className="hamburger" onClick={toggleMenu}>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+        </div>
+        <ul className="innerlinks">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Industries</a></li>
+            <li><a href="#">AI Software</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Contact Us</a></li>
+        </ul>
+        </div>
+    </nav>
+  )
 }
